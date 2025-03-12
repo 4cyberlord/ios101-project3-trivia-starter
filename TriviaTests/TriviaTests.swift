fd57f987909d9277371759f8cea6c38a2f1d7203
4cyberlord <4cyberlord@proton.me>
2025-03-12 14:47:42 -0400

    Initial Commit

---
+++ TriviaTests/TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaTests.swift
+//  TriviaTests
+//
+//  Created by Cyberlord on 3/12/25.
+//
+
+import Testing
+@testable import Trivia
+
+struct TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
